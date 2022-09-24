ea8ebe73843a2db0cfa77cec0c014652a658367a
GitHub <noreply@github.com>
2022-09-24 12:20:01 +0000

    Auto deploy from GitHub Actions build 512
    taoky: [3a660cf] Ch02: wordpress.sh updated for clearer explanation

--- Ch02/wordpress.sh
+++ Ch02/wordpress.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-set -e
+set -eu
 
-your_password=$(tr -dc a-z0-9A-Z < /dev/urandom | head -c 8)
+# Is user root?
+if [ "$(id -u)" != "0" ]; then
+    echo "本脚本需以 root 身份运行"
+    echo "在大多数情况下，可以在你执行脚本的命令前加上 sudo 来以 root 身份运行"
+    exit 1
+fi
 
+PASSWORD_FILE="/root/mysql_wordpress_password.txt"
+
+echo "安装有关依赖：Wordpress, PHP, MySQL 与 Apache"
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y wordpress php libapache2-mod-php mysql-server php-mysql
 
+echo "在 Apache 中添加 Wordpress 站点"
 cat > /etc/apache2/sites-available/wordpress.conf << EOF
 Alias /blog /usr/share/wordpress
 <Directory /usr/share/wordpress>
@@ -24,30 +33,34 @@ Alias /blog /usr/share/wordpress
 </Directory>
 EOF
 
+echo "在 Apache 中启用 Wordpress 站点与 rewrite 模块，并启动 apache2"
 a2ensite wordpress
 a2enmod rewrite
-service apache2 reload
+service apache2 restart
 
-echo "Creating database."
+echo "生成随机数据库密码，并保存至 $PASSWORD_FILE"
+DB_PASSWORD=$(tr -dc a-z0-9A-Z < /dev/urandom | head -c 8)
+echo "${DB_PASSWORD}" > "$PASSWORD_FILE"
+
+echo "启动并创建 MySQL 数据库 wordpress"
+service mysql start
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;"
 
-echo "Configurating privileges."
-mysql -u root -e "CREATE USER IF NOT EXISTS wordpress@localhost IDENTIFIED BY '${your_password}';"
+echo "配置 MySQL 用户 wordpress 相关权限"
+mysql -u root -e "CREATE USER IF NOT EXISTS wordpress@localhost IDENTIFIED BY '${DB_PASSWORD}';"
 mysql -u root -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@localhost;"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
-echo "${your_password}" > /root/mysql_wordpress_password.txt
-
+echo "配置 Wordpress"
 cat > /etc/wordpress/config-localhost.php << EOF
 <?php
 define('DB_NAME', 'wordpress');
 define('DB_USER', 'wordpress');
-define('DB_PASSWORD', '${your_password}');
+define('DB_PASSWORD', '${DB_PASSWORD}');
 define('DB_HOST', 'localhost');
 define('DB_COLLATE', 'utf8mb4_general_ci');
 define('WP_CONTENT_DIR', '/usr/share/wordpress/wp-content');
 ?>
 EOF
 
-service mysql start
-echo "Finished!"
+echo "已完成。使用浏览器打开 http://localhost/blog 以完成最后的配置"
